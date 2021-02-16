92fc073b9adb30605e9a26b691f66c81ed008ec0
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2021-02-16 15:17:35 -0500

    added rendering of multiple error templates and html autoescaping

--- generator.py
+++ generator.py
@@ -1,14 +1,26 @@
-from jinja2 import Environment, FileSystemLoader
 import os
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from errors import error_codes
 
 root = os.path.dirname(os.path.abspath(__file__))
 templates_dir = os.path.join(root, 'templates')
-env = Environment(loader=FileSystemLoader(templates_dir))
+env = Environment(
+	loader=FileSystemLoader(templates_dir),
+	autoescape=select_autoescape(['html'])
+)
 
 for dirName, subdirList, fileList in os.walk(templates_dir):
 	for fname in fileList:
 		template = env.get_template(fname)
 
-		filename = os.path.join(root, 'production', fname)
-		with open(filename, 'w') as fh:
-			fh.write(template.render())
+		if fname == 'error.html':
+			for code, description in error_codes.items():
+				filename = os.path.join(root, 'production', code + '.html')
+
+				with open(filename, 'w') as fh:
+					fh.write(template.render(error_code=code, error_description=description))
+		else:
+			filename = os.path.join(root, 'production', fname)
+
+			with open(filename, 'w') as fh:
+				fh.write(template.render())
