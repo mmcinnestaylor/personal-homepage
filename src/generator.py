1921f95deb1ec3b4783618fe6d80b412e1078fa6
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2021-02-16 20:44:31 -0500

    updated rendering of teaching template

--- src/generator.py
+++ src/generator.py
@@ -1,13 +1,11 @@
 import os
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from errors import error_codes
+from teaching import teaching_appointments
 
 root = os.path.dirname(os.path.abspath(__file__))
 templates_dir = os.path.join(root, 'templates')
-env = Environment(
-	loader=FileSystemLoader(templates_dir),
-	autoescape=select_autoescape(['html'])
-)
+env = Environment(loader=FileSystemLoader(templates_dir))
 
 for dirName, subdirList, fileList in os.walk(templates_dir):
 	for fname in fileList:
@@ -23,4 +21,7 @@ for dirName, subdirList, fileList in os.walk(templates_dir):
 			filename = os.path.join(root, 'production', fname)
 
 			with open(filename, 'w') as fh:
-				fh.write(template.render())
+				if fname == 'teaching.html':
+					fh.write(template.render(teaching_appointments=teaching_appointments))
+				else:
+					fh.write(template.render())
