d8c08714ad301d6e910506994f0618fc3808a55d
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2021-03-08 21:50:29 -0500

    updated for github.io hosting

--- src/errors.py
+++ src/errors.py
@@ -1,13 +1,13 @@
 error_codes = {
-	'400':'Bad Request',
-	'403':'Forbidden',
+	# '400':'Bad Request',
+	# '403':'Forbidden',
 	'404':'Not Found',
-	'405':'Method Not Allowed',
-	'414':'Request-URI Too Long',
-	'416':'Requested Range Not Satisfiable',
-	'500':'Internal Server Error',
-	'501':'Not Implemented',
-	'502':'Bad Gateway',
-	'503':'Service Unavailable',
-	'504':'Gateway Timeout'
+	# '405':'Method Not Allowed',
+	# '414':'Request-URI Too Long',
+	# '416':'Requested Range Not Satisfiable',
+	# '500':'Internal Server Error',
+	# '501':'Not Implemented',
+	# '502':'Bad Gateway',
+	# '503':'Service Unavailable',
+	# '504':'Gateway Timeout'
 }
--- src/generator.py
+++ src/generator.py
@@ -1,24 +1,30 @@
 import os
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader
 from errors import error_codes
 from teaching import teaching_appointments
 
+
 root = os.path.dirname(os.path.abspath(__file__))
+parent = os.path.abspath(os.path.join(root, os.pardir))
 templates_dir = os.path.join(root, 'templates')
+production_dir = os.path.join(parent, 'docs')
+
 env = Environment(loader=FileSystemLoader(templates_dir))
 
 for dirName, subdirList, fileList in os.walk(templates_dir):
 	for fname in fileList:
 		template = env.get_template(fname)
 
-		if fname == 'error.html':
+		if fname == 'base.html':
+			continue
+		elif fname == 'error.html':
 			for code, description in error_codes.items():
-				filename = os.path.join(root, 'production', code + '.html')
+				filename = os.path.join(production_dir, code + '.html')
 
 				with open(filename, 'w') as fh:
 					fh.write(template.render(error_code=code, error_description=description))
 		else:
-			filename = os.path.join(root, 'production', fname)
+			filename = os.path.join(production_dir, fname)
 
 			with open(filename, 'w') as fh:
 				if fname == 'teaching.html':
