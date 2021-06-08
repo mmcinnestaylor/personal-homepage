5ff63879eefb71bbbae636ea315fa511a09f2965
Marlan McInnes-Taylor <mmcinnestaylor@protonmail.com>
2021-06-08 11:42:58 -0400

    moved files to v1 folder

--- v1/teaching.py
+++ v1/teaching.py
@@ -4,7 +4,8 @@ teaching_appointments = {
 		{
 			'course':'CGS 2100 Microcomputer Applications for Business and Economics',
 			'semester':'Spring', 
-			'position':'Undergraduate Teaching Assistant'
+			'position':'Undergraduate Teaching Assistant',
+			'website': None
 		},
 	],
 	'2020':
@@ -12,12 +13,14 @@ teaching_appointments = {
 		{
 			'course':'COP 3363 Programming I for Majors',
 			'semester':'Spring',
-			'position':'Undergraduate Teaching Assistant'
+			'position':'Undergraduate Teaching Assistant',
+			'website': None
 		},
 		{
 			'course':'CGS 2100 Microcomputer Applications for Business and Economics',
 			'semester':'Fall',
-			'position':'Undergraduate Teaching Assistant'
+			'position':'Undergraduate Teaching Assistant',
+			'website': None
 		}
 	],
 	'2019':
@@ -25,12 +28,14 @@ teaching_appointments = {
 		{
 			'course':'COP 3014 Programming I for Non-Majors',
 			'semester':'Spring',
-			'position':'Undergraduate Teaching Assistant'
+			'position':'Undergraduate Teaching Assistant',
+			'website': None
 		},
 		{
 			'course':'COP 3014 Programming I for Non-Majors',
 			'semester':'Fall',
-			'position':'Undergraduate Teaching Assistant'
+			'position':'Undergraduate Teaching Assistant',
+			'website': None
 		}
 	],
 	'2018':
@@ -38,12 +43,14 @@ teaching_appointments = {
 		{
 			'course':'CGS 2100 Microcomputer Applications for Business and Economics',
 			'semester':'Spring',
-			'position':'Undergraduate Teaching Assistant'
+			'position':'Undergraduate Teaching Assistant',
+			'website': None
 		},
 		{
 			'course':'COP 3014 Programming I for Non-Majors',
 			'semester':'Fall',
-			'position':'Undergraduate Teaching Assistant'
+			'position':'Undergraduate Teaching Assistant',
+			'website': None
 		}
 	]
 }
